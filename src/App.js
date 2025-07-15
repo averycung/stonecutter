184e842e2cb111ceb207c1a2a6f4666fb6ff3512
avery <averycung@gmail.com>
2025-07-15 10:46:10 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className='flex justify-center my-7 items-center p-2 m-2 mx-auto'>
+    <h1 className='text-stone-600 font-bold text-6xl'>Stonecutter</h1>
+    </div>
+  );
+}
+
+export default App;
